9903ba514087f88331cd00acb4dd6f1929900de3
Peter Stjern <peter.stjern@p-bolaget.goteborg.se>
2018-02-14 14:57:09 +0100

    Just for keeps

--- js/app.js
+++ js/app.js
@@ -16,32 +16,46 @@ var Model = {
             return userList().push(user);
         })
     },
+
 }
 
 var ViewModel = function() {
     var self = this;
+    var data = ko.observableArray([])
+    
+    Model.addToUserList().forEach(function(user){
+        data().push(user);
+    });
+
 
 }
 
 var apiCall = {
     baseUrl: "https://wind-bow.gomix.me/twitch-api/",
 
-    ajaxCall: function(string) {
-        $.getJSON(`${this.string}`)
+    //pass in user, channels or streams as "type" parameter and a valid user as "user" parameter. 
+    //The functions then returns a json data object.
+    ajaxCall: function(type, user) {
+        $.getJSON(`${this.baseUrl}${type}/${user}?callback=?`)
         .done(function(data){
-            console.log(data);
-        })
+            return data
+        });
     },
 };
 
-var TwitchUser = function() {
+// var TwitchUser = function() {
+
+// }
+
+var TwitchChannel = {
+    getChannel: function(user){
+        return apiCall.ajaxCall("channel", user);
+    },
 
 }
 
-var TwitchChannel = function() {
+// var TwitchStream = function() {
 
-}
+// }
 
-var TwitchStream = function() {
-
-}
+ko.applyBindings(ViewModel);
